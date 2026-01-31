2a23be480e4423c5a420ea19291c712e3fec7fc1
Nemomsa Dereje <nemomsadereje123@gmail.com>
2026-01-31 17:45:59 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
+
 // IMPORTANT: Replace with your actual Firebase project configuration
 // You can find this in your project's settings in the Firebase console.
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
+  projectId: "studio-8807061841-4e5fb",
+  appId: "1:471566651222:web:fd82ea81062d8f7b3e10f7",
+  apiKey: "AIzaSyDiDE3gY5EhlmlzsXQaUUebNkuWS6DdsWs",
+  authDomain: "studio-8807061841-4e5fb.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "471566651222",
 };
