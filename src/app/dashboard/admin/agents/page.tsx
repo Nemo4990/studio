7ab0235a6d74fb37ae89e39bb1088ae101ff9ae4
Nemomsa Dereje <nemomsadereje123@gmail.com>
2026-02-07 22:50:11 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/dashboard/admin/agents/page.tsx
+++ src/app/dashboard/admin/agents/page.tsx
@@ -37,7 +37,7 @@ import {
   writeBatch,
 } from 'firebase/firestore';
 import { MoreHorizontal, PlusCircle, QrCode, Copy } from 'lucide-react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import type { Agent } from '@/lib/types';
 import {
   DropdownMenu,
